a18e5087be6193ac036cdf81ed92ece1df8c9309
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-08-29 09:17:33 -0500

    Programa para medir tiempo, archivo de 10 millones de numeros y ajustes de minimos cuadrados

--- Practice 01/03_InsertionSort.cpp
+++ Practice 01/03_InsertionSort.cpp
@@ -1,6 +1,10 @@
 /*Insertion Sort*/
-
 #include <iostream>
+/*gcc archivo.c -c: Generar .o*/
+/*para compilar: gcc nombre_main.c archivo.o -o programa
+  o sin usar directamente el .o
+  				 gcc nombre_main.c archivo.c -o programa
+ */
 
 using namespace std;
 typedef vector<int> vii;
