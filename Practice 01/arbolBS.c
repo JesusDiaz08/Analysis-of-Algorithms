1e5e81aa882e68e6fd246797e6cd87e0bea7f95d
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-08-30 23:44:07 -0500

    Implementacion de árbol
    Se implementaron algunas de los metodos del arbol

---
+++ Practice 01/arbolBS.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "arbolBS.h"
+
+int index = 0;
+
+/*
+Nombre:			init
+Parámetros:		tree* a
+Retorno:		void
+Descripción:	La función se encargará inicializar
+				el apuntador al árbol
+*/
+void init(tree* a){
+	*a = NULL;
+	return;
+}
+
+/*
+Nombre:			isEmpty
+Parámetros:		tree* a
+Retorno:		int
+Descripción:	La función se encargará verificar
+				si el árbol está vacío: 1 si lo está
+				y 0 para caso contrario.
+*/
+int isEmpty(tree* a){
+	return (a==NULL);
+}
+
+/*
+Nombre:			newNode
+Parámetros:		tree* a, int valor
+Retorno:		void
+Descripción:	La función se encarga de crear un nodo de manera
+				recursiva
+*/
+void newNode(tree* a, int valor){
+	if(isEmpty(a)){			/*Si el nodo está vacio, se crea el nodo raíz*/
+		*a = createNode(valor);
+	}else{					/*Ya hay un nodo previamente creado*/
+		/*Se verifica hacia que lado hay que mover
+		la posición para asignarlo.*/
+		if((*a)->valor < valor){	/*Si es mayor al dato del nodo actual va
+									hacia la derecha*/
+			newNode(&((*a)->right),valor);
+		}else						/*Si es mayor al dato del nodo actual va
+									hacia la derecha*/
+			newNode(&((*a)->left),valor);
+	}
+	return;
+}
+
+
+/*
+Nombre:			createNode();
+Parámetros:		int valor
+Retorno:		Node* 
+Descripción:	La función se encarga de crear un nuevo nodo
+				asignandole el valor que es enviado por parametro
+*/
+Node* createNode(int valor){
+	Node * tmp = (Node *)malloc(sizeof(Node));
+	tmp->left = NULL;
+	tmp->right = NULL;
+	tmp->valor = valor;
+	return tmp;
+}
+
+/*
+Nombre:			fill_inOrden();
+Parámetros:		int valor
+Retorno:		Node* 
+Descripción:	La función se encarga de crear un nuevo nodo
+				asignandole el valor que es enviado por parametro
+*/
--- Practice 01/arbolBS.h
+++ Practice 01/arbolBS.h
@@ -1,12 +1,24 @@
 /*
 La estructura de un nodo será la siguiente
 	valor: 	Se almacenara el valor a ingresar
-	der:	Hace referencia al nodo del lado derecho que esta debajo de él.
-	izq:	Hace referencia al nodo del lado izquierdo que esta debajo de él.
+	rigth:	Hace referencia al nodo del lado derecho que esta debajo de él.
+	left:	Hace referencia al nodo del lado izquierdo que esta debajo de él.
+Asimismo los nodos:
+	tree: 	Hace referencia al árbol a crear
+	pos:	Especifica la posición del apuntador
 */
 
-typedef struct nodo{
+typedef struct node{
 	int valor;
-	struct nodo* der;
-	struct nodo* izq;
-} Nodo;
+	struct node* left;
+	struct node* right;
+} Node;
+
+typedef Node *pos;
+typedef Node *tree;
+
+void init(tree* a);
+int isEmpty(tree* a);
+void newNode(tree* a, int data);
+Node* createNode(int valor);
+void fill_inOrden(tree* a, int* array);
