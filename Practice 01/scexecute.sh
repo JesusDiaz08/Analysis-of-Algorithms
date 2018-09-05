cf0d7d5de220e4062731c27ec58c3fb2a5660681
dan1ela <danilalalaw@gmail.com>
2018-09-04 19:45:49 -0500

    Entradas script excepto Bubble's

--- Practice 01/scexecute.sh
+++ Practice 01/scexecute.sh
@@ -41,10 +41,92 @@ echo "Insertion Sort" >> insertion.txt
 echo " " >> insertion.txt
 ./insertion 100 < Numeros/numeros10millones.txt >> insertion.txt
 ./insertion 1000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 5000 < Numeros/numeros10millones.txt >> insertion.txt
 ./insertion 10000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 50000 < Numeros/numeros10millones.txt >> insertion.txt
 ./insertion 100000 < Numeros/numeros10millones.txt >> insertion.txt
-./insertion 500000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 200000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 400000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 600000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 800000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 100000 < Numeros/numeros10millones.txt >> insertion.txt
 ./insertion 1000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 2000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 3000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 4000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 5000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 6000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 7000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 8000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 9000000 < Numeros/numeros10millones.txt >> insertion.txt
+./insertion 10000000 < Numeros/numeros10millones.txt >> insertion.txt
 
 
+echo "Selection Sort" >> selection.txt
+echo " " >> selection.txt
+./selection 100 < Numeros/numeros10millones.txt >> selection.txt
+./selection 1000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 5000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 10000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 50000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 100000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 200000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 400000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 600000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 800000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 1000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 2000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 3000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 4000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 5000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 6000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 7000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 8000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 9000000 < Numeros/numeros10millones.txt >> selection.txt
+./selection 10000000 < Numeros/numeros10millones.txt >> selection.txt
 
+echo "Binary Search Tree" >> binary_search_tree.txt
+echo " " >> binary_search_tree.txt
+./bst 100 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 1000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 5000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 10000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 50000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 100000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 200000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 400000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 600000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 800000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 1000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 2000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 3000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 4000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 5000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 6000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 7000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 8000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 9000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+./bst 10000000 < Numeros/numeros10millones.txt >> binary_search_tree.txt
+
+echo "Shell Sort" >> shell.txt
+echo " " >> shell.txt
+./shell 100 < Numeros/numeros10millones.txt >> shell.txt
+./shell 1000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 5000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 10000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 50000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 100000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 200000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 400000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 600000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 800000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 1000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 2000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 3000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 4000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 5000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 6000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 7000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 8000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 9000000 < Numeros/numeros10millones.txt >> shell.txt
+./shell 10000000 < Numeros/numeros10millones.txt >> shell.txt
