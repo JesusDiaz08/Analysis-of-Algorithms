862e1afc4b70750ee67615cf31f4ab867ef8e397
elpatsan <elpatsan@gmail.com>
2018-12-02 12:48:49 -0600

    Agrego p5js y empiezo animacion

--- Proyecto final/js/script.js
+++ Proyecto final/js/script.js
@@ -1,4 +1,6 @@
 var numeros = [];
+var color = [];
+var animar = false;
 
 function showEntrada() {
     let elementos = document.getElementById("inputValues").value;
@@ -11,4 +13,33 @@ function showEntrada() {
 		numeros.push(parseInt(tokens[j]));
 	}
 	fieldNameElement.innerHTML = numeros [0];
+	animar = true;
+	noLoop();
+}
+
+
+function setup() 
+{
+  createCanvas(700, 400);  
+}
+
+function draw() 
+{ 
+	if (animar){
+		let j;
+		drawArray (numeros, 10, 10, 40);
+		animar = false;
+	} 
+}
+
+function drawArray (arr, x, y, size){
+	let i;
+	let posx;
+	for (i = 0; i < arr.length; i++){
+		posx = x + size * i; 
+		rect (posx, y, size, size);
+		stroke(125);
+		text (numeros[i], posx + (size/2), y + (size/2));
+		stroke(0);
+	}
 }
